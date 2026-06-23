7480f9a727a7a5915371a4c5966d64f96f916c66
vale690 <valeryscausilberrio@gmail.com>
2026-06-23 10:02:25 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+// Seleccionar todas las secciones
+const secciones = document.querySelectorAll("section");
+
+// Ocultar todas las secciones
+function ocultarSecciones() {
+    secciones.forEach(seccion => {
+        seccion.style.display = "none";
+    });
+}
+
+// Mostrar una sección
+function mostrarSeccion(id) {
+    ocultarSecciones();
+    document.getElementById(id).style.display = "block";
+}
+
+// Mostrar Inicio al cargar la página
+mostrarSeccion("inicio");
+
+// Botones del menú
+const botones = document.querySelectorAll(".menu button");
+
+botones[0].addEventListener("click", () => {
+    mostrarSeccion("inicio");
+});
+
+botones[1].addEventListener("click", () => {
+    mostrarSeccion("conceptos");
+});
+
+botones[2].addEventListener("click", () => {
+    mostrarSeccion("caracteristicas");
+});
+
+botones[3].addEventListener("click", () => {
+    mostrarSeccion("funciones");
+});
+
+botones[4].addEventListener("click", () => {
+    mostrarSeccion("tabla");
+});
+
+botones[5].addEventListener("click", () => {
+    mostrarSeccion("examen");
+});
